ee70cf7c1a4f2845b409ca47fdb5da6f5b3f59a4
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-14 01:55:52 +0600

    nested component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
 // function App(){
 //   return <> Hello </>
 // }
 
-const App = () => {
-  return React.createElement("h1",{}, "Hello World");
-};
+// const App = () => {
+//   return React.createElement("h1",{}, "Hello World");
+// };
 
+
+const App = () => {
+  return (
+    <>
+      <MyName />
+      <h1 className="h1">Hello World</h1>
+      <p>Awesome {1+1}</p>
+      <MyName />
+  </>
+  );
+}
+
+const MyName = () => {
+  return <h3>Faisal Ahmed</h3>;
+}
 
 
 export default App
